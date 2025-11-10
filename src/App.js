3efd7583127e4f8a945016d86e5f48c85e169e21
hajoonpk <nouveau486@naver.com>
2025-11-10 12:00:45 +0900

    20251110version1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          교수님살려주세요 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
